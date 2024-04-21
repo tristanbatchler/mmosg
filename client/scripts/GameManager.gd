5bcd73114e30b61a7e5a9c62e815f0ce5b17c981
Tristan Batchler <batchlertristan@gmail.com>
2024-04-21 11:27:28 +1000

    Refactor client project file structure

---
+++ client/scripts/GameManager.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const PlayerScene: PackedScene = preload("res://core/player/Player.tscn")
+const ActorScene: PackedScene = preload("res://core/actor/Actor.tscn")
+
+var player_pid: String
+var others_pids: Dictionary = {}
+
+func _ready():
+	NetworkClient.connect("packet_received", _on_network_client_packet_received)
+
+func _on_network_client_packet_received(p_type: String, p_data: Dictionary):
+	var from_pid: String = Marshalls.raw_to_base64(p_data["from_pid"])
+
+	match p_type:
+		"Pid":
+			player_pid = from_pid
+			print("Player PID: ", player_pid)
+
+
+		"Hello":
+			var state_view: Dictionary = p_data["state_view"]
+			var x: float = state_view["x"]
+			var y: float = state_view["y"]
+			var z: float = state_view["z"]
+			var name: String = state_view["name"]
+
+			if from_pid == player_pid:
+				var player: Node3D = PlayerScene.instantiate().init(Vector3(x, y, z), name)
+				get_tree().root.add_child(player)
+
+			elif not others_pids.has(from_pid):
+				var actor: CharacterBody3D = ActorScene.instantiate().init(from_pid, Vector3(x, y, z), name)
+				others_pids[from_pid] = actor
+				get_tree().root.add_child(actor)
+
+			else:
+				printerr("Already know %s but got another HelloPacket from it" % from_pid)
+			
+
+		"Targetlocation":
+			var x: float = p_data["x"]
+			var y: float = p_data["y"]
+			var z: float = p_data["z"]
+
+			if others_pids.has(from_pid):
+				others_pids[from_pid].navigate_to(Vector3(x, y, z))
+
+		_:
+			print("Unknown packet type: ", p_type)
+	
---
+++ client/scripts/NetworkClient.gd
@@ -0,0 +1,48 @@
+extends Node
+
+const Msgpack = preload("res://scripts/Msgpack.gd")
+static var EVERYONE: String = "AAAAAAAAAAAAAAAAAAAAAA=="
+
+var socket := WebSocketPeer.new() 
+
+signal server_connected()
+signal server_error(err: int)
+signal packet_received(p_type: String, p_data: Dictionary)
+signal packet_error(err: int)
+
+@export var hostname: String = "localhost"
+@export var port: int = 8081
+
+func _ready() -> void:
+	#var cert: X509Certificate = X509Certificate.new()
+	#cert.load("res://rootCA.crt")
+	var tls_options: TLSOptions = TLSOptions.client()
+	var err: int = socket.connect_to_url("wss://%s:%d" % [hostname, port], tls_options)
+	if err:
+		printerr("Unable to connect")
+		set_process(false)
+		server_error.emit(err)
+	else:
+		server_connected.emit()
+
+func _process(delta) -> void:
+	socket.poll()
+
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			var packet: Dictionary = Msgpack.decode(socket.get_packet())["result"]
+			var p_type: String = packet.keys()[0]
+			var p_data: Dictionary = packet[p_type]
+			packet_received.emit(p_type, p_data)
+			print("Received packet %s" % packet)
+
+func send_packet(packet: Dictionary) -> void:
+	var data: PackedByteArray = Msgpack.encode(packet)["result"]
+	var err: int = socket.send(data)
+	if err:
+		printerr("Error sending data. Error code: ", err)
+		set_process(false)
+		packet_error.emit(err)
+		
+func _exit_tree():
+	socket.close()
